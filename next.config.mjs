3b24c9d44c8e91280fee8f83df32e631ecf3b68e
Kris-Young-Kim <youngkiss3181@gmail.com>
2025-11-26 16:05:00 +0900

    webback감시제외

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        ...(config.watchOptions?.ignored || []),
+        "C:/DumpStack.log.tmp",
+        "C:/hiberfil.sys",
+        "C:/pagefile.sys",
+        "C:/swapfile.sys",
+      ],
+    }
+    return config
+  },
 }
 
 export default nextConfig
